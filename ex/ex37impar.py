ed6ec3051f4ca18a17c27f182eae61c440cf1f97
Jon Waldir Quispe Marca <jonwaldir59@gmail.com>
2026-04-23 09:50:37 -0300

    Update ex37impar.py

--- ex/ex37impar.py
+++ ex/ex37impar.py
@@ -1,46 +1,33 @@
 import os
-def grava_arq(sequencia,par,pasta,):
 
+def valida_impar(numero):
+    if numero % 2 != 0:
+        return numero
+    else:
+        return -1
+
+def procedimento_leitura(caminho):
+    if not os.path.exists(caminho):
+        print("Erro: Arquivo não encontrado.")
+        return
+
+    print("--- Números Ímpares ---")
     
-    
-    if not os.path.exists(pasta):
-        os.makedirs(pasta)
-    caminho_completo = os.path.join(pasta, "ex37.txt")
-
-    with open(caminho_completo,'a') as arquivo:
-        arquivo.write(f"{sequencia}\n")
-        arquivo.write(f"{par} -1\n")
-
+    with open(caminho, 'r') as arquivo:
+        for linha in arquivo:
+            itens = linha.split()
+            for i in itens:
+                valor = int(i)
+                resultado = valida_impar(valor)
+                if resultado != -1:
+                    print(resultado)
 
 def main():
-    pasta_alvo = 'C:\\temp\\'
-    nome_arquivo = 'ex37.txt'
+    pasta = 'C:\\temp\\'
+    arquivo = 'ex37.txt'
+    caminho_total = pasta + arquivo
 
-    caminho_total = os.path.join(pasta_alvo, nome_arquivo)
-
-    if os.path.exists(caminho_total):
-        os.remove(caminho_total)
-
-    contador = 1
-    t1 = -1
-    t2 = 1
-    impar: int = 0
-    nao_impar: int =0
-    
-    while contador < 100 :
-        proximo = t1+t2
-        if proximo %2 == 0 :
-        
-            nao_impar = proximo
-        else:
-            impar = proximo
-        
-        grava_arq(impar,nao_impar, pasta_alvo)
-        t1 = t2
-        t2 = proximo
-        
-        contador +=1
-
+    procedimento_leitura(caminho_total)
 
 if __name__ == "__main__":
     main()
